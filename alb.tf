5c233a09120747dcb36b9a2dc94b1568b86d176c
pramoth <83818338+pramothar@users.noreply.github.com>
2023-03-31 15:42:26 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 # Creating External LoadBalancer
 resource "aws_lb" "external-alb" {
-  name               = "External LB"
+  name               = "ExternalLB"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.demosg.id]
@@ -8,7 +8,7 @@ resource "aws_lb" "external-alb" {
 }
 
 resource "aws_lb_target_group" "target-elb" {
-  name     = "ALB TG"
+  name     = "ALBTG"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.demovpc.id
